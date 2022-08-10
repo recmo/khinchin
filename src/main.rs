9c277321817bf34791cf7ba6d67070d15f5ef1c5
Remco Bloemen <remco@wicked.ventures>
2022-08-10 12:20:11 -0700

    Bench k0

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,11 @@ use rug::{
 use std::{
     cmp::max,
     f64::consts::{LN_2, TAU},
+    fs::{self, File},
+    io::Read,
+    ops::Neg,
     sync::atomic::{AtomicU64, Ordering},
-    time::{Duration, Instant}, fs::{File, self}, io::Read, ops::Neg,
+    time::{Duration, Instant},
 };
 
 static SIEVE: Lazy<Sieve> = Lazy::new(|| {
@@ -24,11 +27,12 @@ static SIEVE: Lazy<Sieve> = Lazy::new(|| {
     Sieve::new(hi as usize)
 });
 
-static TABLE_TIME: AtomicU64 = AtomicU64::new(0);
+static UPDATE_TIME: AtomicU64 = AtomicU64::new(0);
 static SUM_TIME: AtomicU64 = AtomicU64::new(0);
-static ZETA_TIME: AtomicU64 = AtomicU64::new(0);
-static INNER_TIME: AtomicU64 = AtomicU64::new(0);
+static TABLE_TIME: AtomicU64 = AtomicU64::new(0);
+static SERIES_TIME: AtomicU64 = AtomicU64::new(0);
 static DIVSUM_TIME: AtomicU64 = AtomicU64::new(0);
+static ZETA_TIME: AtomicU64 = AtomicU64::new(0);
 
 fn zeta(precision: u32, n: u32) -> Float {
     Float::with_val(precision, Float::zeta_u(n))
@@ -84,7 +88,7 @@ impl ZetaTable {
         };
         let k_max = result.k_max();
         result.table.reserve_exact(k_max);
-        for k in 3..k_max as u32 {
+        for k in 0..k_max as u32 {
             result.table.push(result.compute_term(k));
         }
         result
@@ -93,17 +97,11 @@ impl ZetaTable {
     pub fn zeta_m1(&mut self, n: u32) -> Float {
         assert!(n >= self.n);
         assert!(n % 2 == 0);
+
         let now = Instant::now();
-
-        // 2^(-n)
-        let mut z = Float::new(2);
-
         while self.n < n {
             self.n += 2;
             let k_upper = self.k_max();
-
-            // 2^(-n)
-            z = Float::with_val(self.precision + 1 - n, Float::i_exp(1, -(n as i32)));
 
             // Update table
             self.table.truncate(k_upper);
@@ -116,15 +114,22 @@ impl ZetaTable {
                     let precision = self.precision.saturating_sub(leading_zeros).max(32);
                     term.set_prec(precision);
                     *term /= k * k;
-                    z += &*term;
                 });
         }
-        TABLE_TIME.fetch_add(now.elapsed().as_nanos() as u64, Ordering::Relaxed);
- 
+        UPDATE_TIME.fetch_add(now.elapsed().as_nanos() as u64, Ordering::Relaxed);
+
+        // 2^(-n)
+        let now = Instant::now();
+        let mut z = Float::with_val(self.precision, Float::i_exp(1, -(self.n as i32)));
+        self.table.iter().skip(3).for_each(|term| {
+            z += term;
+        });
+        SUM_TIME.fetch_add(now.elapsed().as_nanos() as u64, Ordering::Relaxed);
+
         // Test
         // let expected: Float = zeta_sum(self.precision, n) - 1;
-        // let error = (&z - expected).abs().to_f64().log2();
-        // dbg!(error);
+        // let accuracy = (&z - expected).get_exp().unwrap_or_default().neg();
+        // dbg!(accuracy);
 
         z
     }
@@ -134,6 +139,9 @@ impl ZetaTable {
     }
 
     fn compute_term(&self, k: u32) -> Float {
+        if k < 3 {
+            return Float::new(2);
+        }
         let leading_zeros = ((k as f64).log2() * (self.n as f64) - 1.0).floor() as u32;
         let precision = self.precision.saturating_sub(leading_zeros).max(32);
         let term = Float::with_val(precision, Float::u_pow_u(k, self.n)).recip();
@@ -166,7 +174,8 @@ impl ZetaBoost {
         if let Some(table) = &mut self.table {
             let now = Instant::now();
             let result = table.zeta_m1(n);
-            ZETA_TIME.fetch_add(now.elapsed().as_nanos() as u64, Ordering::Relaxed);
+            // let result = zeta_sum(self.precision, n) - 1;
+            TABLE_TIME.fetch_add(now.elapsed().as_nanos() as u64, Ordering::Relaxed);
             return result;
         }
 
@@ -191,7 +200,7 @@ impl ZetaBoost {
         } else {
             zeta_sum(inner_precision, n)
         };
-        INNER_TIME.fetch_add(now.elapsed().as_nanos() as u64, Ordering::Relaxed);
+        SERIES_TIME.fetch_add(now.elapsed().as_nanos() as u64, Ordering::Relaxed);
 
         // Compute the factor
         self.update_factor(n);
@@ -263,8 +272,11 @@ fn k0(precision: u32) -> Float {
     let mut zeta_boost = ZetaBoost::new(precision);
     for n in 2_u64.. {
         let s1_prev = s1.clone();
-        s2 -= Rational::from((1, (2 * n - 2) * (2 * n - 1) ));
-        s1 += &s2 * zeta_boost.zeta_m1(2 * n as u32) / n;
+        s2 -= Rational::from((1, (2 * n - 2) * (2 * n - 1)));
+        let now = Instant::now();
+        let z = zeta_boost.zeta_m1(2 * n as u32);
+        ZETA_TIME.fetch_add(now.elapsed().as_nanos() as u64, Ordering::Relaxed);
+        s1 += &s2 * z / n;
         if s1 == s1_prev {
             break;
         }
@@ -293,8 +305,11 @@ fn bench_zeta(precision: u32) {
 fn bench_k0() {
     let expected = reference();
 
-    println!("target_bits actual_bits table_time zeta_time inner_time divsum_time total_time");
-    for target_bits in (1..25).map(|i| i * 4000) {
+    println!(
+        "target_bits actual_bits update_time sum_time table_time series_time divsum_time \
+         zeta_time total_time"
+    );
+    for target_bits in (1..=100).map(|i| i * 4000) {
         reset();
 
         // Compute
@@ -305,21 +320,28 @@ fn bench_k0() {
         // Measure accuracy
         let actual_bits = (k0 - &expected).get_exp().unwrap().neg();
 
-        println!("{} {} {} {} {} {} {}", target_bits, actual_bits,
+        println!(
+            "{} {} {} {} {} {} {} {} {}",
+            target_bits,
+            actual_bits,
+            Duration::from_nanos(UPDATE_TIME.load(Ordering::Relaxed)).as_secs_f64() * 1000.0,
+            Duration::from_nanos(SUM_TIME.load(Ordering::Relaxed)).as_secs_f64() * 1000.0,
             Duration::from_nanos(TABLE_TIME.load(Ordering::Relaxed)).as_secs_f64() * 1000.0,
-            Duration::from_nanos(ZETA_TIME.load(Ordering::Relaxed)).as_secs_f64() * 1000.0,
-            Duration::from_nanos(INNER_TIME.load(Ordering::Relaxed)).as_secs_f64() * 1000.0,
+            Duration::from_nanos(SERIES_TIME.load(Ordering::Relaxed)).as_secs_f64() * 1000.0,
             Duration::from_nanos(DIVSUM_TIME.load(Ordering::Relaxed)).as_secs_f64() * 1000.0,
+            Duration::from_nanos(ZETA_TIME.load(Ordering::Relaxed)).as_secs_f64() * 1000.0,
             total_time.as_secs_f64() * 1000.0,
         );
     }
 }
 
 fn reset() {
+    UPDATE_TIME.store(0, Ordering::Relaxed);
     TABLE_TIME.store(0, Ordering::Relaxed);
-    ZETA_TIME.store(0, Ordering::Relaxed);
-    INNER_TIME.store(0, Ordering::Relaxed);
+    SUM_TIME.store(0, Ordering::Relaxed);
+    SERIES_TIME.store(0, Ordering::Relaxed);
     DIVSUM_TIME.store(0, Ordering::Relaxed);
+    ZETA_TIME.store(0, Ordering::Relaxed);
 }
 
 fn reference() -> Float {
